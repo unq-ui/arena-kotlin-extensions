f1ee044c48e4a40f3e50961cd82457f187ca2ad4
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2019-08-01 20:13:19 -0300

    Column extensions

--- src/main/java/examples/extensions/widgets/control/TableExtensionsWindow.kt
+++ src/main/java/examples/extensions/widgets/control/TableExtensionsWindow.kt
@@ -1,5 +1,6 @@
 package examples.extensions.widgets.control
 
+import java.awt.Color
 import org.uqbar.arena.kotlin.extensions.*
 import org.uqbar.arena.widgets.Panel
 import org.uqbar.arena.widgets.tables.Column
@@ -23,10 +24,20 @@ fun main () = TableExtensionsWindow().startApplication()
  */
 class TableExtensionsWindow : MainWindow<TableExtensionsWindow.AppModel>(AppModel()) {
     @Observable class AppModel {
-        var items = listOf(Item(1, "A"), Item(2, "B"), Item(3, "C"), Item(4, "D"))
+        var items = listOf(
+                Item(1, "A", "Coso A"),
+                Item(2, "B", "Coso B"),
+                Item(3, "C", "Coso C"),
+                Item(4, "D", "Coso D")
+        )
         var selected = items[2]
     }
-    @Observable class Item(val id: Int, val name: String)
+    @Suppress("unused")
+    @Observable
+    class Item(val id: Int, val name: String, val description: String) {
+        val blue: Color = Color.BLUE
+        val yellow: Color = Color.YELLOW
+    }
     override fun createContents(mainPanel: Panel) {
         title = "Table Extension Window"
 
@@ -35,8 +46,28 @@ class TableExtensionsWindow : MainWindow<TableExtensionsWindow.AppModel>(AppMode
         table bindSelectionTo "selected"
         table.visibleRows = 5
 
-        Column<Item>(table).setTitle("ID").bindContentsToProperty("id")
-        Column<Item>(table).setTitle("Name").bindContentsToProperty("name")
+        val c1 = Column<Item>(table)
+        c1.title = "ID"
+        c1.fixedSize = 30
+        c1 bindContentsTo "id"
+        c1 bindBackgroundTo "blue"
+        c1 bindColorTo "yellow"
+
+        val c2 = Column<Item>(table)
+        c2.background = Color.WHITE
+        c2.color = Color.BLACK
+        c2.weight = 50
+        c2.setTitle("Name")
+        c2.align("center")
+        c2.bindContentsToProperty("name")
+
+        val c3 = Column<Item>(table)
+        c3.title = "Name2"
+        c3.background = Color.BLACK
+        c3.color = Color.WHITE
+        c3.fontSize = 12
+        c3.align("right")
+        c3.bindContentsToProperty("description")
     }
 
 }
--- src/main/java/org/uqbar/arena/kotlin/extensions/Table.kt
+++ src/main/java/org/uqbar/arena/kotlin/extensions/Table.kt
@@ -1,7 +1,12 @@
 package org.uqbar.arena.kotlin.extensions
 
+import java.awt.Color
 import org.uqbar.arena.widgets.Control
 import org.uqbar.arena.widgets.tables.Table
+import org.uqbar.arena.widgets.tables.Column
+import org.uqbar.arena.widgets.tables.labelprovider.BackgroundProvider
+import org.uqbar.arena.widgets.tables.labelprovider.ForegroundProvider
+import org.uqbar.arena.widgets.tables.labelprovider.PropertyLabelProvider
 import org.uqbar.lacar.ui.model.ControlBuilder
 import org.uqbar.lacar.ui.model.TableBuilder
 import org.uqbar.lacar.ui.model.bindings.Binding
@@ -36,3 +41,55 @@ var Table<*>.visibleRows: Int
  *  - getItemType
  *  - getColumns
  */
+
+/**
+ * Column
+ */
+
+var Column<*>.title: String?
+    get() = null
+    set(value) {
+        value?.let { this.setTitle(value) }
+    }
+var Column<*>.weight: Int?
+    get() = null
+    set(value) {
+        value?.let { this.setWeight(value) }
+    }
+var Column<*>.fixedSize: Int?
+    get() = null
+    set(value) {
+        value?.let { this.setFixedSize(value) }
+    }
+var Column<*>.background: Color?
+    get() = null
+    set(value) {
+        value?.let { this.setBackground(value) }
+    }
+var Column<*>.color: Color?
+    get() = null
+    set(value) {
+        value?.let { this.setForeground(value) }
+    }
+var Column<*>.fontSize: Int?
+    get() = null
+    set(value) {
+        value?.let { this.setFont(value) }
+    }
+
+infix fun Column<*>.align(position: String): Column<*> =
+        when (position.toLowerCase()) {
+            "left" -> this.alignLeft()
+            "right" -> this.alignRight()
+            "center" -> this.alignCenter()
+            else -> this.alignLeft()
+        }
+
+infix fun Column<*>.bindContentsTo(propertyName: String): PropertyLabelProvider<*> =
+        this.bindContentsToProperty(propertyName)
+
+infix fun Column<*>.bindBackgroundTo(propertyName: String): BackgroundProvider<*, Color> =
+        this.bindBackground(propertyName)
+
+infix fun Column<*>.bindColorTo(propertyName: String): ForegroundProvider<*, Color> =
+        this.bindForeground(propertyName)
